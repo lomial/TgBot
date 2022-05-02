512dd64ffe235dddc2b614cc71d6c94d9374f49a
lomial <lomial2000@gmail.com>
2022-05-02 13:41:36 +0300

    main

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ networkurl = "http://194.87.103.77:63500/Network"
 
 # Telegram bot token from botfather on telegram
 
-token = "1174119629:AAHQKxdoNIjs_zonL5wgV6y8jzfPwBVVnds" 
+token = "5334913763:AAFtE5uxi0Rywc1Gihn_8t9An4-89-ZDPkY" 
 
 #Telegram Chatid 
 
