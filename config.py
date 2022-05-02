ade3a85d3459f79b96fa8535bc8f7c4f1864b7a0
lomial <lomial2000@gmail.com>
2022-05-02 22:44:27 +0300

    main

--- config.py
+++ config.py
@@ -7,15 +7,15 @@ networkurl = "http://194.87.103.77:63500/Network"
 
 # Telegram bot token from botfather on telegram
 
-token = "1174119629:AAHQKxdoNIjs_zonL5wgV6y8jzfPwBVVnds" 
+token = "5334913763:AAFtE5uxi0Rywc1Gihn_8t9An4-89-ZDPkY" 
 
 #Telegram Chatid 
 
-chatid = -1001794249068
+chatid = "-1001794249068"
 
 #Telegram backend messages for person running the bot 
 
-chatiddev = -1001794249068
+chatiddev = "-1001794249068"
 
 #operational parameters
 refreshinterval = 1
