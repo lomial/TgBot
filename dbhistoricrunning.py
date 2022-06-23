657093dc63f9c0a81f3ab77010ef46ddf258be9e
lomial <lomial2000@gmail.com>
2022-06-23 14:35:29 +0300

    main

--- dbhistoricrunning.py
+++ dbhistoricrunning.py
@@ -13,7 +13,7 @@ import nodeinformation
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 running = True
-debug = False
+debug = True
 
 def main(interval,debug):
     global variable
@@ -27,7 +27,9 @@ def main(interval,debug):
         
         def check():        
             global variable
-            countuser = sum(MainmoduleSQL.getcountuser())     
+            countuser = sum(MainmoduleSQL.getcountuser())
+            #logging.info("countuser")
+            #logging.info(countuser)     
             datasql = MainmoduleSQL.getdatauser()
             #countuser = sum(countuser) #преобразование в нужный тип данных tuple -> int
             
@@ -36,7 +38,7 @@ def main(interval,debug):
                 logging.info(datasql)
             
             
-            for i in range(countuser):
+            for i in range(countuser+1):
                 #print ("i=",i)
                 try:
                     time.sleep(0)
@@ -44,66 +46,68 @@ def main(interval,debug):
                     activestatus = int(datasql[i][12])
                     
                     if debug:
-                        #logging.info("activestatus")
-                        #logging.info(activestatus)
+                        logging.info("activestatus")
+                        logging.info(activestatus)
                         
-                        logging.info("keyretrieve")
-                        logging.info(keyretrieve)
+                    logging.info("keyretrieve")
+                    logging.info(keyretrieve)
                     
                     if debug:
                         logging.info("key is send to module")
-                        #logging.info(MainmoduleSQL.checkifpresent)
+                        logging.info(MainmoduleSQL.checkifpresent)
                         
-                        try:
-                            #logging.info(MainmoduleSQL.runningvalue)
-                            #logging.info(activestatus)
-                            onlinenodedata = nodeinformation.getnodeinformation(keyretrieve) 
+                    try:
+                        logging.info("keyretrieve")
+                        logging.info(MainmoduleSQL.runningvalue)
+                        logging.info("activestatus")
+                        logging.info(activestatus)
+                        onlinenodedata = nodeinformation.getnodeinformation(keyretrieve) 
                             
                             
-                            if onlinenodedata[6] == "Yes":
-                                runningvalue = 1
+                        if onlinenodedata[6] == "Yes":
+                            runningvalue = 1
                                 
-                            elif onlinenodedata[6] == "No":
-                                runningvalue = 0
+                        elif onlinenodedata[6] == "No":
+                            runningvalue = 0
                             
                             
-                            if activestatus < runningvalue:
+                        if activestatus < runningvalue:
                                 
-                                if debug:
-                                    logging.info("node is up")
-                                MainmoduleSQL.updatestatus(keyretrieve,runningvalue)
-                                telegramclient.up(keyretrieve)
-                                if MainmoduleSQL.usernode == True:
-                                    telegramclient.userup(MainmoduleSQL.chatidvar,keyretrieve)
+                            if debug:
+                                logging.info("node is up")
+                            MainmoduleSQL.updatestatus(keyretrieve,runningvalue)
+                            telegramclient.up(keyretrieve)
+                            if MainmoduleSQL.usernode == True:
+                                telegramclient.userup(MainmoduleSQL.chatidvar,keyretrieve)
                         
-                            if activestatus > runningvalue:
+                        if activestatus > runningvalue:
                                 
-                                if debug:
-                                    logging.info("node is down")  
-                                MainmoduleSQL.updatestatus(keyretrieve,runningvalue)
-                                telegramclient.down(keyretrieve)
-                                if MainmoduleSQL.usernode == True:
-                                    telegramclient.userdown(MainmoduleSQL.chatidvar,keyretrieve) 
-                            else:
-                                if debug:
-                                    logging.info("nothing changed")
+                            if debug:
+                                logging.info("node is down")  
+                            MainmoduleSQL.updatestatus(keyretrieve,runningvalue)
+                            telegramclient.down(keyretrieve)
+                            if MainmoduleSQL.usernode == True:
+                                telegramclient.userdown(MainmoduleSQL.chatidvar,keyretrieve) 
+                        else:
+                            if debug:
+                                logging.info("nothing changed")
                                 
                                 
-                        except Exception as E:
-                            logging.info('Error : {}'.format(E))     
+                    except Exception as E:
+                        logging.info('Error : {}'.format(E))     
                
         
                 except Exception as E:
                     logging.info('Error : {}'.format(E)) 
                     
-        if variable < countuser:
+        if variable < countuser+1:
             logging.info("Going to next str")
             time.sleep(2)
             variable = variable + 1
             check()
             
             
-        if variable == countuser:
+        if variable == countuser+1:
             logging.info("reached max srt Starting at str 1")
             time.sleep(2)
             variable = 0
